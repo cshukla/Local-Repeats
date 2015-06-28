deae39698aced940105dc6d274f5d855be73c5b8
Chinmay Shukla <cshukla@fas.harvard.edu>
2015-06-28 15:51:45 -0400

    Separate a GTF file by chromosomes

---
+++ bin/shell_scripts/seperate_chrom.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+grep -w 'chr1' results/5-15-15/gencode_moran_merged_catalog.genes.gtf > results/5-17-15/chr1_merged_catalog.genes.gtf
+grep -w 'chr2' results/5-15-15/gencode_moran_merged_catalog.genes.gtf > results/5-17-15/chr2_merged_catalog.genes.gtf
+grep -w 'chr3' results/5-15-15/gencode_moran_merged_catalog.genes.gtf > results/5-17-15/chr3_merged_catalog.genes.gtf
+grep -w 'chr4' results/5-15-15/gencode_moran_merged_catalog.genes.gtf > results/5-17-15/chr4_merged_catalog.genes.gtf
+grep -w 'chr5' results/5-15-15/gencode_moran_merged_catalog.genes.gtf > results/5-17-15/chr5_merged_catalog.genes.gtf
+grep -w 'chr6' results/5-15-15/gencode_moran_merged_catalog.genes.gtf > results/5-17-15/chr6_merged_catalog.genes.gtf
+grep -w 'chr7' results/5-15-15/gencode_moran_merged_catalog.genes.gtf > results/5-17-15/chr7_merged_catalog.genes.gtf
+grep -w 'chr8' results/5-15-15/gencode_moran_merged_catalog.genes.gtf > results/5-17-15/chr8_merged_catalog.genes.gtf
+grep -w 'chr9' results/5-15-15/gencode_moran_merged_catalog.genes.gtf > results/5-17-15/chr9_merged_catalog.genes.gtf
+grep -w 'chr10' results/5-15-15/gencode_moran_merged_catalog.genes.gtf > results/5-17-15/chr10_merged_catalog.genes.gtf
+grep -w 'chr11' results/5-15-15/gencode_moran_merged_catalog.genes.gtf > results/5-17-15/chr11_merged_catalog.genes.gtf
+grep -w 'chr12' results/5-15-15/gencode_moran_merged_catalog.genes.gtf > results/5-17-15/chr12_merged_catalog.genes.gtf
+grep -w 'chr13' results/5-15-15/gencode_moran_merged_catalog.genes.gtf > results/5-17-15/chr13_merged_catalog.genes.gtf
+grep -w 'chr14' results/5-15-15/gencode_moran_merged_catalog.genes.gtf > results/5-17-15/chr14_merged_catalog.genes.gtf
+grep -w 'chr15' results/5-15-15/gencode_moran_merged_catalog.genes.gtf > results/5-17-15/chr15_merged_catalog.genes.gtf
+grep -w 'chr16' results/5-15-15/gencode_moran_merged_catalog.genes.gtf > results/5-17-15/chr16_merged_catalog.genes.gtf
+grep -w 'chr17' results/5-15-15/gencode_moran_merged_catalog.genes.gtf > results/5-17-15/chr17_merged_catalog.genes.gtf
+grep -w 'chr18' results/5-15-15/gencode_moran_merged_catalog.genes.gtf > results/5-17-15/chr18_merged_catalog.genes.gtf
+grep -w 'chr19' results/5-15-15/gencode_moran_merged_catalog.genes.gtf > results/5-17-15/chr19_merged_catalog.genes.gtf
+grep -w 'chr20' results/5-15-15/gencode_moran_merged_catalog.genes.gtf > results/5-17-15/chr20_merged_catalog.genes.gtf
+grep -w 'chr21' results/5-15-15/gencode_moran_merged_catalog.genes.gtf > results/5-17-15/chr21_merged_catalog.genes.gtf
+grep -w 'chr22' results/5-15-15/gencode_moran_merged_catalog.genes.gtf > results/5-17-15/chr22_merged_catalog.genes.gtf
+grep -w 'chrX' results/5-15-15/gencode_moran_merged_catalog.genes.gtf > results/5-17-15/chrX_merged_catalog.genes.gtf
+grep -w 'chrY' results/5-15-15/gencode_moran_merged_catalog.genes.gtf > results/5-17-15/chrY_merged_catalog.genes.gtf
