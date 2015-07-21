916b39042c42e0ce2405341de0a05a57d80c56e8
Chinmay Shukla <cshukla@fas.harvard.edu>
2015-07-21 07:32:46 -0400

    Make box plot, violin plot, CDF plot and density plot to compare Hi-C interactions across conditions

--- bin/r_scripts/compare_hi_c_interactions.R
+++ bin/r_scripts/compare_hi_c_interactions.R
@@ -2,5 +2,30 @@ library(ggplot2)
 
 ca = commandArgs(trailing=T)
 df.file = ca[1]
+output.pre = ca[2]
 
-df = read.table(df.file, header=T, quote="\"")
+df = read.table(df.file, header=F, quote="\"")
+colnames(df) = c("Average.Interactions", "Label")
+ggplot(df, aes(x=Average.Interactions, color=Label)) + 
+	geom_density() + 
+	theme_bw()
+
+ggsave(paste0(output.pre, "_density.pdf"))
+
+ggplot(df, aes(x=Average.Interactions, color=Label)) +
+	stat_ecdf() +
+	theme_bw()
+
+ggsave(paste0(output.pre, "_cdf.pdf"))
+
+ggplot(df, aes(x=Label, y=Average.Interactions, color=Label)) +
+	geom_boxplot() +
+	theme_bw()
+
+ggsave(paste0(output.pre, "_boxplot.pdf"))
+
+ggplot(df, aes(x=Label, y=Average.Interactions, color=Label)) +
+	geom_violin() +
+	theme_bw()
+
+ggsave(paste0(output.pre, "_violin.pdf"))
